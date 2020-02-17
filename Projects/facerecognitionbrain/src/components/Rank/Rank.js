437f4455af18a6801336ac7babfb8b28363f0bba
Krishan-Naidoo <56049699+Krishan-Naidoo@users.noreply.github.com>
2020-02-18 01:38:04 +0200

    Uploading new ReactApp Project FaceRecognitionBrain

---
+++ Projects/facerecognitionbrain/src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+	return (
+		<div>
+			<div className ='white f3'>
+				{`Krishan, Your current rank is ...`}
+			</div>
+			<div className ='white f5'>
+				{`#1`}
+			</div>
+		</div>
+	);
+}
+
+export default Rank;
